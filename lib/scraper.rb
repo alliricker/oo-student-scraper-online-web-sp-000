e79fc896840bda02de5dc4fbbe4ac0a955af3f7c
Allison Ricker <alliricker@gmail.com>
2020-11-03 17:06:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,16 +1,20 @@
-require 'nokogiri'
+
 require 'open-uri'
 require 'pry'
 
-html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
-Nokogiri::HTML(html)
-doc = Nokogiri::HTML(html)
-puts doc
-binding.pry
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-
+    def self.scrape_index_page(index_url)
+      html = open(index_url)
+      doc = Nokogiri::HTML(html)
+      student_cards = doc.css(".student-card a")
+      student_cards.collect do |element|
+        {:name => element.css(".student-name").text ,
+          :location => element.css(".student-location").text,
+          :profile_url => element.attr('href')
+        }
   end
 
   def self.scrape_profile_page(profile_url)
