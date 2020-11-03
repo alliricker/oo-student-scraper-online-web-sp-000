deba3715c3de20b57d3b4614ffa30c38a261888f
Allison Ricker <alliricker@gmail.com>
2020-11-03 17:24:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,13 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @student_hash = student_hash 
+    self.send("name=", student_hash[:name])
+    #@location = student_hash[:location]
+    self.send("location=", student_hash[:location])
+    #@profile_url = student_hash[:profile_url]
+    self.send("profile_url=", student_hash[:profile_url])
+    @@all << self
+
 
 
   end
