bf5dafd02e7a8f4fce48bd2da03f420dd0f42322
Allison Ricker <alliricker@gmail.com>
2020-11-03 16:54:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+html = open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
+
 class Scraper
 
   def self.scrape_index_page(index_url)
